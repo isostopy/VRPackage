f489bbb16be43cde9e2be99d503e68279891f927
Sergio <software@isostopy.com>
2024-04-26 12:59:29 +0200

    Guardar HandPose en asset
    Ahora el componente HandPose tiene un botón que permite guardar la pose que tiene como un asset.

--- Grab/Editor/Code/HandPoseComponentEditor.cs
+++ Grab/Editor/Code/HandPoseComponentEditor.cs
@@ -4,6 +4,7 @@ using UnityEditor;
 namespace Isostopy.VR.Grab.Editor
 {
 	[CustomEditor(typeof(HandPose))]
+	[CanEditMultipleObjects]
 	public class HandPoseComponentEditor : UnityEditor.Editor
 	{
 		int pickerId = 0;
@@ -11,14 +12,32 @@ namespace Isostopy.VR.Grab.Editor
 		public override void OnInspectorGUI()
 		{
 			base.OnInspectorGUI();
-
 			EditorGUILayout.Space();
+
+			// Seleccionar pose desde un asset.
 			if (GUILayout.Button("Set from asset"))
 			{
-				pickerId = EditorGUIUtility.GetObjectPickerControlID() + 1;
-				EditorGUIUtility.ShowObjectPicker<HandPoseAsset>(null, false, "", pickerId);
+				OpenObjectPicker();
 			}
+			PoseHandAsObjectPicked();
 
+			// Guardar la pose actual como un nuevo asset.
+			if (GUILayout.Button("Save to asset"))
+			{
+				SaveCurrentPoseInAsset();
+			}
+		}
+
+		/// <summary> Abre el object picker de Unity para seleccionar un asset. </summary>
+		void OpenObjectPicker()
+		{
+			pickerId = EditorGUIUtility.GetObjectPickerControlID() + 1;
+			EditorGUIUtility.ShowObjectPicker<HandPoseAsset>(null, false, "", pickerId);
+		}
+
+		/// <summary> Posar la mano como indica el asset seleccionado en el object picker. </summary>
+		void PoseHandAsObjectPicked()
+		{
 			if (Event.current.commandName == "ObjectSelectorUpdated"
 				&& EditorGUIUtility.GetObjectPickerControlID() == pickerId)
 			{
@@ -28,10 +47,10 @@ namespace Isostopy.VR.Grab.Editor
 			}
 		}
 
+		/// <summary> Coloca el modelo de la mano en la pose que esta guardada en un asset. </summary>
 		void PoseHand(HandPoseAsset asset)
 		{
 			var target = this.target as HandPose;
-
 
 			var assetBones = asset.GetBonesDictionary();
 			var targetBones = target.bones;
@@ -42,12 +61,33 @@ namespace Isostopy.VR.Grab.Editor
 				if (assetBones.ContainsKey(name) == false)
 					continue;
 
-				Undo.RecordObject(bone.transform, "Changed Hand Pose");
+				Undo.RecordObject(bone.transform, "Changed Hand Pose of " + target.transform.name);
 
 				Pose pose = assetBones[name];
 				bone.transform.SetLocalPositionAndRotation(pose.position, pose.rotation);
 			}
+		}
 
+		/// <summary> Guarda la pose en la que esta la mano en un asset en el proyecto. </summary>
+		void SaveCurrentPoseInAsset()
+		{
+			var target = this.target as HandPose;
+
+			HandPoseAsset asset = ScriptableObject.CreateInstance<HandPoseAsset>();
+
+			foreach (var bone in target.bones)
+			{
+				asset.AddPose(bone.name, bone.transform.localPosition, bone.transform.localRotation);
+			}
+
+			string path = EditorUtility.SaveFilePanel("Create Hand Pose Asset", "Assets/", "new Hand Pose", "asset");
+
+			if (string.IsNullOrEmpty(path))
+				return;
+
+			path = System.IO.Path.GetRelativePath(Application.dataPath, path);
+			path = "Assets/" + path;
+			AssetDatabase.CreateAsset(asset, path);
 		}
 	}
 }
